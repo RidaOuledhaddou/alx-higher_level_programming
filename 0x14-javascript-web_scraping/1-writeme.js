8ede9efd513a4182563db498ca617bb11e9921c4
RidaOuledhaddou <ouledhaddourida@gmail.com>
2024-04-23 04:34:38 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
-try {
-  fs.writeFileSync(file, string, { encoding: 'utf8' });
-} catch (err) {
-  console.error(err);
-}
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) {
+    console.log(error);
+  }
+});
