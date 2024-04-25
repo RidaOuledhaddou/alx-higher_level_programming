efd5dd39856136effdba15aa50269da2b15b23f4
RidaOuledhaddou <ouledhaddourida@gmail.com>
2024-04-25 12:35:34 -0700

    0x12-javascript-warm_up

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
-const rd = require('rd');
-const a = rd.readFileSync(process.argv[2], 'utf8');
-const b = rd.readFileSync(process.argv[3], 'utf8');
-rd.writeFileSync(process.argv[4], a + b);
+const fs = require('fs').promises;
+const { argv } = require('process');
+const fileA = argv[2];
+const fileB = argv[3];
+const fileC = argv[4];
+async function concatTwoFiles () {
+  try {
+    const data1 = await fs.readFile(fileA);
+    const data2 = await fs.readFile(fileB);
+    fs.writeFile(fileC, data1 + data2);
+  } catch (err) {
+    console.log(err);
+  }
+}
+concatTwoFiles();
