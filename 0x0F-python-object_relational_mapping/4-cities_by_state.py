a8d33cc86beb3bb9c389ab878fa4640bbda170a4
RidaOuledhaddou <ouledhaddourida@gmail.com>
2024-04-26 02:12:18 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
+"""states models
 """
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-This time the script is safe from
-MySQL injections!
-"""
+    db_host = "localhost"
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    port = 3306
 
-import MySQLdb
-from sys import argv
+    db = MySQLdb.connect(
+        host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
+    )
+    cursor = db.cursor()
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON states.id = cities.state_id \
+    ORDER BY cities.id")
+    rows = cursor.fetchall()
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    for row in rows:
+        print(row)
 
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                *
-            FROM
-                states
-            WHERE
-                name LIKE BINARY %(name)s
-            ORDER BY
-                states.id ASC
-        """, {
-            'name': argv[4]
-        })
-
-        rows = cur.fetchall()
-
-    if rows is not None:
-        for row in rows:
-            print(row)
+    cursor.close()
+    db.close()
